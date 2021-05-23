efab9b2f72ba37da8cff8e5dba6ff234343fe5d0
ekoziol-bain <eric.koziol@bain.com>
2021-05-23 14:08:24 -0400

    updated views

--- calc.py
+++ calc.py
@@ -14,5 +14,32 @@ def calc_loan_monthly_payment(principal, rate, term):
 def total_payment(monthly_payment, term):
     return monthly_payment * term
 
-def target_interest_rate(monthly_payment, term):
-    False
+
+def create_mortage_range(principal, term, rmax=0.1, rstep=0.00125):
+    df = pd.DataFrame(data={'rate':np.arange(0,rmax+rstep,rstep)})
+    df['monthly_payment'] = df.apply(lambda x: calc_loan_monthly_payment(principal, x['rate'], term), axis=1)
+    df['total_payment'] = df.apply(lambda x: total_payment(x['monthly_payment'], term), axis=1)
+    return df
+
+def find_target_interest_rate(principal, term, target_payment):
+    df = create_mortage_range(principal, term, rmax=0.185, rstep=0.00125)
+    idx = df.loc[df['monthly_payment'] < target_payment, 'monthly_payment'].idxmax()
+    return df.iloc[idx,:]['rate']
+
+def amount_remaining(principal, monthly_payment, rate, months_remaining):
+    '''
+    B = (PMT/R) x (1 - (1/(1+R)^N) In the formula, "B" is the principal balance, 
+    "PMT" is the monthly payment for principal and interest and "N" is the number of months remaining.
+    "R" is your interest rate, but it's expressed as a monthly rate rather than an annual one. 
+    To get the monthly rate, simply take your annual rate, expressed as a decimal, and divide it by 12. 
+    If your annual rate is 6 percent, for example: R = 0.06/12 = 0.005.
+    '''
+    return (monthly_payment/(rate/12)) * (1 - (1/(1+(rate/12))**months_remaining))
+
+def create_mortgage_table(principal, rate, term):
+    df = pd.DataFrame(data={'month':np.arange(0,term+1,1)})
+    monthly_payment = calc_loan_monthly_payment(principal, rate, term)
+    
+    df['months_remaining'] = term - df['month']                                     
+    df['amount_remaining'] = df['months_remaining'].map(lambda x: amount_remaining(principal, monthly_payment, rate, x))
+    return df
--- index.py
+++ index.py
@@ -37,7 +37,7 @@ app.layout = html.Div(className='main', children=[
         html.Div(id="minimum_potential_payment"),
 
         html.P("Remaining Principal"),
-        dcc.Input(id="remaining_principal", type="number", placeholder="Remaining Principal", value=340492.76, debounce = True),
+        dcc.Input(id="remaining_principal", type="number", placeholder="Remaining Principal", value=220446.5, debounce = True),
         html.Br(),
 
         html.P("Remaining Term in Months (20 years = 240 months)"),
@@ -55,6 +55,10 @@ app.layout = html.Div(className='main', children=[
                 dcc.Input(id="target_term", type="number", placeholder="Target Term", value=360, debounce = True),
                 html.Br(),
 
+                html.P("Target Interest Rate"),
+                dcc.Input(id="target_rate", type="number", placeholder="Target Interest Rate", value=0.02, debounce = True),
+                html.Br(),
+
                 html.Div(id="Required Interest Rate"),
 
             ])
@@ -63,9 +67,9 @@ app.layout = html.Div(className='main', children=[
         dcc.Graph(
         id='monthly_payment_graph'
         ),
-        # dcc.Graph(
-        # id='total_payment_graph'
-        # ),
+        dcc.Graph(
+        id='future_payment_graph'
+        ),
         ], style={'display': 'inline-block'})
     ]
 )
@@ -90,17 +94,14 @@ def update_minimum_potential_payment(current_principal, current_rate, current_te
     return u'Minimum Potential Monthly Payment {}'.format(calc_loan_monthly_payment(current_principal, 0.0, current_term))
 
 
-def create_mortage_range(principal, term, rmax=0.1, rstep=0.0125):
-    df = pd.DataFrame(data={'rate':np.arange(0,rmax+rstep,rstep)})
-    df['monthly_payment'] = df.apply(lambda x: calc_loan_monthly_payment(principal, x['rate'], term), axis=1)
-    df['total_payment'] = df.apply(lambda x: total_payment(x['monthly_payment'], term), axis=1)
-    return df
-
-
-def create_value_plot(principal, term, x='rate', y='monthly_payment', title='Monthly Payment by Interest Rate'):
+def create_staged_value_plot(principal, remaining_principal, term, x='rate', y='monthly_payment', title='Monthly Payment by Interest Rate'):
     df = create_mortage_range(principal, term)
+    dfc = create_mortage_range(remaining_principal, term)
+
     fig = px.line(df, x=x, y=y, title=title)
+    fig.append_trace({'x':dfc[x],'y':dfc[y],'type':'scatter','name':'Remaining Principal'},1,1)
     return fig
+
 
 @app.callback(
     Output("monthly_payment_graph", "figure"),
@@ -119,8 +120,199 @@ def update_total_payment_graph(current_principal, current_term):
     return create_value_plot(current_principal, current_term, y='total_payment', title='Total Payment by Interest Rate')
 
 
+@app.callback(
+    Output("future_payment_graph", "figure"),
+    Input("current_principal", "value"),
+    Input("remaining_principal", "value"),
+    Input("current_rate", "value"),
+    Input("target_rate", "value"),
+    Input("current_term", "value"),
+    Input("remaining_term", "value"),
+    Input("target_term", "value"),
+
+)
+def update_future_payment_graph(current_principal, 
+                                remaining_principal,
+                                current_rate, 
+                                target_rate,
+                                current_term,
+                                remaining_term, 
+                                target_term):
+    return understand_mortgage_extension(original_principal=current_principal, 
+                                        remaining_principal=remaining_principal, 
+                                        original_rate=current_rate, 
+                                        new_rate=target_rate,
+                                        original_term=current_term,
+                                        remaining_term=remaining_term, 
+                                        new_term=target_term)
+
 #need to show target monthly payment
 #calculate required interest rate
+
+#create plot of remaining principal vs interest rate for target monthly payment
+##show how it would extend to X number of years later
+
+#### Plotting functions ####
+def create_value_plot(principal, 
+                      term, 
+                      x='rate', 
+                      y='monthly_payment', 
+                      title='Monthly Payment by Interest Rate'):
+    df = create_mortage_range(principal, term)
+    fig = px.line(df, x=x, y=y, title=title)
+    
+    #current values
+    fig.add_shape(type='line',
+                x0=0,
+                y0=2015.11,
+                x1=0.0250,
+                y1=2015.11,
+                line=dict(color='Black',),
+                xref='x',
+                yref='y'
+    )
+    
+    fig.add_shape(type='line',
+                x0=0.0250,
+                y0=1000,
+                x1=0.0250,
+                y1=2015.11,
+                line=dict(color='Black',),
+                xref='x',
+                yref='y'
+    )
+    
+    #target values
+    fig.add_shape(type='line',
+                x0=0,
+                y0=1699.58,
+                x1=0.0125,
+                y1=1699.58,
+                line=dict(color='Red',),
+                xref='x',
+                yref='y'
+    )
+    
+    fig.add_shape(type='line',
+                x0=0.0125,
+                y0=1000,
+                x1=0.0125,
+                y1=1699.58,
+                line=dict(color='Red',),
+                xref='x',
+                yref='y'
+    )
+
+    return fig
+
+
+def create_staged_value_plot(principal, 
+                            remaining_principal, 
+                            current_term,
+                            current_rate,
+                            target_term,
+                            target_monthly_payment,
+                            x='rate', 
+                            y='monthly_payment', 
+                            title='Monthly Payment by Interest Rate'):
+    df = create_mortage_range(principal, current_term)
+    dfc = create_mortage_range(remaining_principal, target_term)
+
+    target_interest_rate_current = find_target_interest_rate(principal, term, target_monthly_payment)
+    target_interest_rate_refi = find_target_interest_rate
+
+    fig = px.line(df, x=x, y=y, title=title)
+    fig.append_trace({'x':dfc[x],'y':dfc[y],'type':'scatter','name':'Remaining Principal'},1,1)
+    
+    ##curent rate lines
+    #horizontal line
+    fig.add_shape(type='line',
+                x0=0,
+                y0=2015.11,
+                x1=0.0250,
+                y1=2015.11,
+                line=dict(color='Black',),
+                xref='x',
+                yref='y'
+    )
+    
+    #vertical line
+    fig.add_shape(type='line',
+                x0=current_rate,
+                y0=1000,
+                x1=current_rate,
+                y1=2015.11,
+                line=dict(color='Black',),
+                xref='x',
+                yref='y'
+    )
+    
+    ##target values
+    #horizontal
+    fig.add_shape(type='line',
+                x0=0,
+                y0=1699.58,
+                x1=0.0125,
+                y1=1699.58,
+                line=dict(color='Red',),
+                xref='x',
+                yref='y'
+    )
+    #vertical
+    fig.add_shape(type='line',
+                x0=target_interest_rate_current,
+                y0=1000,
+                x1=target_interest_rate_current,
+                y1=1699.58,
+                line=dict(color='Red',),
+                xref='x',
+                yref='y'
+    )
+    
+    return fig
+
+
+def understand_mortgage_extension(original_principal, 
+                                  remaining_principal, 
+                                  original_rate, 
+                                  new_rate,
+                                  original_term,
+                                  remaining_term, 
+                                  new_term):
+    print("variable values")
+    print(original_principal, 
+          remaining_principal, 
+          original_rate, 
+          new_rate,
+          original_term,
+          remaining_term, 
+          new_term)
+    print("----")
+
+    df_original = create_mortgage_table(original_principal, original_rate, original_term)
+    print("original",df_original)
+    df_refi = create_mortgage_table(remaining_principal, new_rate, new_term)
+    print("refi", df_refi)
+    df_refi['month'] = df_refi['month'] + remaining_term
+    
+    df_original_pre = df_original[df_original['month']<=remaining_term]
+    df_original_post = df_original[df_original['month']>remaining_term]
+    
+    fig = px.line(df_original_pre, x='month', y='amount_remaining')
+    fig.append_trace({'x':df_refi['month'],
+                      'y':df_refi['amount_remaining'],
+                      'type':'scatter',
+                      'line':{'dash': 'solid', 'color': 'green'},
+                      'name':'Current Refinance Scenario'},1,1)
+    fig.append_trace({'x':df_original_post['month'],
+                      'y':df_original_post['amount_remaining'],
+                      'type':'scatter',
+                      'line':{'dash': 'dash', 'color': 'blue'},
+                      'name':'Original Loan'},1,1)
+    
+    
+    return fig
+
 
 if __name__ == "__main__":
     app.run_server("0.0.0.0", debug=True, port=8050)
