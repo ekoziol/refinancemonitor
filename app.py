082a5ccfdbda76ca530b585db14235cca9a564d7
ekoziol <e.trip.k@gmail.com>
2021-05-30 19:45:26 -0400

    try dash change for heroku

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from calc import *
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__)#, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 app.config.suppress_callback_exceptions = True
 # money = FormatTemplate.money(2)
